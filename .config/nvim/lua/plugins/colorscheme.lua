61034f2d4de16c1fd56fd80288e06a1f7b03c812
agoratopia <264286985+agoratopia@users.noreply.github.com>
2026-07-22 00:05:17 -0400

    Switch colorscheme to kanagawa-dragon

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,5 +4,5 @@ vim.pack.add { gh 'rebelot/kanagawa.nvim' }
 ---@diagnostic disable-next-line: missing-fields
 require('kanagawa').setup {}
 
--- Other variants: kanagawa-dragon (darker), kanagawa-lotus (light)
-vim.cmd.colorscheme 'kanagawa-wave'
+-- Other variants: kanagawa-wave (default/mid-contrast), kanagawa-lotus (light)
+vim.cmd.colorscheme 'kanagawa-dragon'
